3d28576ae6e9fda1a897bb8ca4b248084ace1add
haijindaga <ueeee327@gmail.com>
2026-08-22 23:55:37 +0900

    Remove depth_mask_to_points from exports

--- src/panda_handover/__init__.py
+++ src/panda_handover/__init__.py
@@ -1,12 +1,10 @@
 """Panda tool-handover experiment support package."""
 
 from .capture import RgbdCapture
-from .geometry import depth_mask_to_points, matrix_from_pose, transform_points
+from .geometry import matrix_from_pose, transform_points
 
 __all__ = [
     "RgbdCapture",
-    "depth_mask_to_points",
     "matrix_from_pose",
     "transform_points",
 ]
-
